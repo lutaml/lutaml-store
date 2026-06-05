2f8b9cd5d47ea8e26e5a61b5d834af2a152c260c
Ronald Tse <ronald.tse@ribose.com>
2026-06-05 12:27:29 +0800

    docs: rewrite README.adoc and fix demo references
    - Rewrite README to document all current features: PackageStore, HttpCache,
      CacheStore, file I/O, model registry, events, error hierarchy
    - Fix Store::Store -> Store::BasicStore in demo files
    - Update spec_helper.rb configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "lutaml/model"
-require "lutaml/store"
+require_relative "../lib/lutaml/store"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -13,4 +13,15 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  def fixture_path(filename)
+    File.expand_path("../fixtures/#{filename}", __FILE__)
+  end
+end
+
+Lutaml::Model::Config.configure do |config|
+  # config.xml_adapter_type = :nokogiri
+  config.json_adapter_type = :standard_json
+  # config.yaml_adapter_type = :standard_yaml
+  # config.toml_adapter_type = :toml_rb
 end
